311295944047b1fc8ac7057c5957845ffa3f0787
Loay Shqair <loay.shqair@gmail.com>
2026-02-14 20:36:05 +0300

    proj add

---
+++ project/Monster.java
@@ -0,0 +1,10 @@
+public class Monster {
+    int row;
+    int col;
+    int distance;
+    public Monster (int row, int col, int distance) {
+        this.row = row;
+        this.col = col;
+        this.distance = distance;
+    }
+}
